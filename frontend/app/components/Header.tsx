afbedf5a629d0160ef794ac1dbb4a2168978cd61
momo-41 <minimizu41@gmail.com>
2024-03-12 20:02:54 +0900

    Headerの作成

---
+++ frontend/app/components/Header.tsx
@@ -0,0 +1,40 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from "@mui/material";
+
+export default function ButtonAppBar() {
+  return (
+    <Box>
+      <AppBar color="default" position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            fontFamily=""
+            letterSpacing={2}
+            sx={{ flexGrow: 1 }}
+          >
+            BookCamp
+          </Typography>
+          <Button color="inherit" sx={{ fontFamily: "" }}>
+            ログイン
+          </Button>
+          <IconButton
+            size="large"
+            color="inherit"
+            aria-label="menu"
+            sx={{ p: "12px" }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
