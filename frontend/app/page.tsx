be0420e92757b8e0c46a3dcfb24187cbfb58a377
momo-41 <minimizu41@gmail.com>
2024-03-16 01:00:46 +0900

    タブパネルの挿入

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import CampusAll from "./components/SearchBox";
+import TabPanel from "./components/TabPanel";
 
 export default function Home() {
   return (
     <>
       <CampusAll />
+      <TabPanel />
     </>
   );
 }
