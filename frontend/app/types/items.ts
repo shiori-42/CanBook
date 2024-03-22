ac0b761c79086497333fc0d0cf11842fa16fb5b9
momo-41 <minimizu41@gmail.com>
2024-03-22 23:50:12 +0900

    ログイン機能の編集・追加

--- frontend/app/types/items.ts
+++ frontend/app/types/items.ts
@@ -1,8 +1,8 @@
-export type item = {
-  id: number;
-  name: string;
-  price: string;
-  sell: string;
-  imagepath: string;
-  //学部入れる？？？？？？？？？？
-};
+// export type item = {
+//   id: number;
+//   name: string;
+//   price: string;
+//   sell: string;
+//   imagepath: string;
+//   //学部入れる？？？？？？？？？？
+// };
