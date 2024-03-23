ab4f5d88f236a8517b098a8558ca46328bbd5353
momo-41 <minimizu41@gmail.com>
2024-03-23 09:27:28 +0900

    ログインした後にできることの確認

--- frontend/app/components/Todo.tsx
+++ frontend/app/components/Todo.tsx
@@ -1,49 +1,49 @@
-// import { FormEvent } from "react";
-// import { useQueryClient } from "@tanstack/react-query";
+import { FormEvent } from "react";
+import { useQueryClient } from "@tanstack/react-query";
 import {
   ArrowRightOnRectangleIcon,
   ShieldCheckIcon,
 } from "@heroicons/react/24/solid";
 import useStore from "../store";
-// import { useQueryTasks } from "../hooks/useQueryTasks";
-// import { useMutateTask } from "../hooks/useMutateTask";
+import { useQueryTasks } from "../hooks/useQueryTasks";
+import { useMutateTask } from "../hooks/useMutateTask";
 import { useMutateAuth } from "../hooks/useMutateAuth";
-// import { TaskItem } from "./TaskItem";
+import { TaskItem } from "./TaskItem";
 
 export const Todo = () => {
-  // const queryClient = useQueryClient();
-  // const { editedTask } = useStore();
-  // const updateTask = useStore((state) => state.updateEditedTask);
-  // const { data, isLoading } = useQueryTasks();
-  // const { createTaskMutation, updateTaskMutation } = useMutateTask();
+  const queryClient = useQueryClient();
+  const { editedTask } = useStore();
+  const updateTask = useStore((state) => state.updateEditedTask);
+  const { data, isLoading } = useQueryTasks();
+  const { createTaskMutation, updateTaskMutation } = useMutateTask();
   const { logoutMutation } = useMutateAuth();
-  // const submitTaskHandler = (e: FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   if (editedTask.id === 0)
-  //     createTaskMutation.mutate({
-  //       title: editedTask.title,
-  //     });
-  //   else {
-  //     updateTaskMutation.mutate(editedTask);
-  //   }
-  // };
+  const submitTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (editedTask.id === 0)
+      createTaskMutation.mutate({
+        title: editedTask.title,
+      });
+    else {
+      updateTaskMutation.mutate(editedTask);
+    }
+  };
   const logout = async () => {
     await logoutMutation.mutateAsync();
-    // queryClient.removeQueries(["tasks"]);
+    queryClient.removeQueries(["tasks"]);
   };
   return (
     <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
-      {/* <div className="flex items-center my-3">
+      <div className="flex items-center my-3">
         <ShieldCheckIcon className="h-8 w-8 mr-3 text-indigo-500 cursor-pointer" />
         <span className="text-center text-3xl font-extrabold">
           Task Manager
         </span>
-      </div> */}
+      </div>
       <ArrowRightOnRectangleIcon
         onClick={logout}
         className="h-6 w-6 my-6 text-blue-500 cursor-pointer"
       />
-      {/* <form onSubmit={submitTaskHandler}>
+      <form onSubmit={submitTaskHandler}>
         <input
           className="mb-3 mr-3 px-3 py-2 border border-gray-300"
           placeholder="title ?"
@@ -66,7 +66,7 @@ export const Todo = () => {
             <TaskItem key={task.id} id={task.id} title={task.title} />
           ))}
         </ul>
-      )} */}
+      )}
     </div>
   );
 };
--- frontend/app/hooks/useQueryTasks.ts
+++ frontend/app/hooks/useQueryTasks.ts
@@ -1,27 +1,27 @@
-// import axios from "axios";
-// import { useQuery } from "@tanstack/react-query";
-// import { useError } from "../hooks/useError";
-// import { Task } from "../types";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { useError } from "../hooks/useError";
+import { Task } from "../types";
 
-// export const useQueryTasks = () => {
-//   const { switchErrorHandling } = useError();
-//   const getTasks = async () => {
-//     const { data } = await axios.get<Task[]>(
-//       `${process.env.NEXT_PUBLIC_API_URLL}/tasks`,
-//       { withCredentials: true }
-//     );
-//     return data;
-//   };
-//   return useQuery<Task[], Error>({
-//     queryKey: ["tasks"],
-//     queryFn: getTasks,
-//     staleTime: Infinity,
-//     onError: (err: any) => {
-//       if (err.response.data.message) {
-//         switchErrorHandling(err.response.data.message);
-//       } else {
-//         switchErrorHandling(err.response.data);
-//       }
-//     },
-//   });
-// };
+export const useQueryTasks = () => {
+  const { switchErrorHandling } = useError();
+  const getTasks = async () => {
+    const { data } = await axios.get<Task[]>(
+      `${process.env.NEXT_PUBLIC_API_URLL}/tasks`,
+      { withCredentials: true }
+    );
+    return data;
+  };
+  return useQuery<Task[], Error>({
+    queryKey: ["tasks"],
+    queryFn: getTasks,
+    staleTime: Infinity,
+    onError: (err: any) => {
+      if (err.response.data.message) {
+        switchErrorHandling(err.response.data.message);
+      } else {
+        switchErrorHandling(err.response.data);
+      }
+    },
+  });
+};
