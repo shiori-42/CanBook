0c1a86a96d39c3f15b323683e01d0db52739fa11
momo-41 <minimizu41@gmail.com>
2024-03-23 09:28:12 +0900

    ログインUIの編集

--- frontend/app/components/Auth.tsx
+++ frontend/app/components/Auth.tsx
@@ -1,6 +1,19 @@
 import { useState, FormEvent } from "react";
 import { CheckBadgeIcon, ArrowPathIcon } from "@heroicons/react/24/solid";
 import { useMutateAuth } from "../hooks/useMutateAuth";
+import {
+  Avatar,
+  Box,
+  Button,
+  Grid,
+  Link,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { teal } from "@mui/material/colors";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import ImportExportIcon from "@mui/icons-material/ImportExport";
 
 export const Auth = () => {
   const [name, setName] = useState("");
@@ -35,19 +48,67 @@ export const Auth = () => {
   };
 
   return (
-    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
-      <div className="flex items-center">
-        <CheckBadgeIcon className="h-8 w-8 mr-2 text-blue-500" />
-        <span className="text-center text-3xl font-extrabold">
-          {/* //タイトル */}
-          Todo app by React/Go(Echo)
-        </span>
-      </div>
-      <h2 className="my-6">{isLogin ? "Login" : "Create a new account"}</h2>
+    <Paper
+      elevation={3}
+      sx={{
+        p: 4,
+        height: { xs: "50vh", sm: "72vh" },
+        width: "280px",
+        m: "15px auto",
+      }}
+    >
+      <Grid
+        container
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="center"
+      >
+        <Avatar sx={{ bgcolor: teal[400] }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography fontSize={20} sx={{ mt: "18px", mb: "28px" }}>
+          {isLogin ? "ログイン" : "会員登録"}
+        </Typography>
+      </Grid>
       <form onSubmit={submitAuthHandler}>
-        <div>
-          {/* //ユーザーがnameを入力するためのところ */}
-          <input
+        <TextField
+          name="name"
+          type="name"
+          label="Username"
+          variant="outlined"
+          fullWidth
+          required
+          autoFocus
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <TextField
+          name="email"
+          type="email"
+          label="Email address"
+          variant="outlined"
+          fullWidth
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          sx={{ mt: 1.5 }}
+        />
+
+        <TextField
+          name="password"
+          type="password"
+          label="Password"
+          variant="outlined"
+          fullWidth
+          required
+          onChange={(e) => setPw(e.target.value)}
+          value={pw}
+          sx={{ mt: 1.5 }}
+        />
+
+        {/* <div> */}
+        {/* //ユーザーがnameを入力するためのところ */}
+        {/* <input
             className="mb-3 px-3 text-sm py-2 border border-gray-300"
             name="name"
             type="name"
@@ -55,11 +116,11 @@ export const Auth = () => {
             placeholder="name"
             onChange={(e) => setName(e.target.value)}
             value={name}
-          />
-        </div>
-        <div>
-          {/* //ユーザーがe-mailを入力するためのところ */}
-          <input
+          /> */}
+        {/* </div> */}
+        {/* <div> */}
+        {/* //ユーザーがe-mailを入力するためのところ */}
+        {/* <input
             className="mb-3 px-3 text-sm py-2 border border-gray-300"
             name="email"
             type="email"
@@ -67,11 +128,11 @@ export const Auth = () => {
             placeholder="Email address"
             onChange={(e) => setEmail(e.target.value)}
             value={email}
-          />
-        </div>
-        <div>
-          {/* //ユーザーがパスワードを入力するためのところ */}
-          <input
+          /> */}
+        {/* </div> */}
+        {/* <div> */}
+        {/* //ユーザーがパスワードを入力するためのところ */}
+        {/* <input
             className="mb-3 px-3 text-sm py-2 border border-gray-300"
             name="password"
             type="password"
@@ -79,22 +140,71 @@ export const Auth = () => {
             onChange={(e) => setPw(e.target.value)}
             value={pw}
           />
-        </div>
-        <div className="flex justify-center my-2">
-          {/* //submitボタンの作成 */}
-          <button
+        </div> */}
+        {/* <div className="flex justify-center my-2"> */}
+        {/* //submitボタンの作成 */}
+        {/* <Typography onClick={() => setIsLogin(!isLogin)}>こちら</Typography> */}
+        <Box mt={4}>
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            fullWidth
+            disabled={!name || !email || !pw}
+          >
+            {isLogin ? "ログイン" : "会員登録"}
+          </Button>
+          {/* <button
             className="disabled:opacity-40 py-2 px-4 rounded text-white bg-indigo-600"
-            disabled={!email || !pw}
+            disabled={!name || !email || !pw}
             type="submit"
           >
             {isLogin ? "Login" : "Sign Up"}
-          </button>
-        </div>
+          </button> */}
+          {/* </div> */}
+        </Box>
       </form>
-      <ArrowPathIcon
-        onClick={() => setIsLogin(!isLogin)}
-        className="h-6 w-6 my-2 text-blue-500 cursor-pointer"
-      />
-    </div>
+      <Box position={"relative"}>
+        <Typography
+          variant="caption"
+          display="block"
+          mt={1}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          アカウントを持っていますか？
+        </Typography>
+        <Link href="#">
+          <Typography
+            fontSize={12}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            アカウントを作成
+          </Typography>
+        </Link>
+        {/* <ImportExportIcon
+          onClick={() => setIsLogin(!isLogin)}
+          // className="h-6 w-6 my-2 text-blue-500 cursor-pointer"
+          width={10}
+          height={10}
+          // sx={{
+          //   display: "flex",
+          //   alignItems: "center",
+          //   justifyContent: "center",
+          //   position: "absolute",
+          // }} //きかない //一旦コメントアウト一旦一旦
+        /> */}
+      </Box>
+    </Paper>
   );
 };
+
+//urlはloginだけでなく、signupのurlも作ってLikタグで飛ばす。また当たらしくsignupのフォルダーを作って、signup/page.tsxを作る。？？？？
+//こちら　の部分を onClick={() => setIsLogin(!isLogin)}　にしたら上手く飛ぶかも、、、？
