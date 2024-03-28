7b280f09bc1e5d986aca53db591e567459f52ae7
shiori-42 <shiori0123.happysmile@gmail.com>
2024-03-28 13:55:11 +0900

    fix_chat

--- go/backend/api/route.go
+++ go/backend/api/route.go
@@ -6,7 +6,7 @@
 /*   By: shiori0123 <shiori0123@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 12:02:10 by shiori0123        #+#    #+#             */
-/*   Updated: 2024/03/22 15:52:13 by shiori0123       ###   ########.fr       */
+/*   Updated: 2024/03/28 02:11:20 by shiori0123       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ import (
 func RegisterRoutes(e *echo.Echo) {
 	handler.RegisterUserRoutes(e)
 	handler.RegisterItemRoutes(e)
+	handler.RegisterWebSocketRoutes(e)
 }
--- go/backend/ws/handler.go
+++ go/backend/ws/handler.go
@@ -6,7 +6,7 @@
 /*   By: shiori0123 <shiori0123@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 23:20:41 by shiori0123        #+#    #+#             */
-/*   Updated: 2024/03/27 23:48:15 by shiori0123       ###   ########.fr       */
+/*   Updated: 2024/03/28 03:49:02 by shiori0123       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@ package ws
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 )
 
 var (
-	clients  = make(map[string]*Client)
+	clients  = make(map[uint]*Client)
 	upgrader = websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool {
 			return true
@@ -32,39 +33,49 @@ var (
 type Handler struct{}
 
 func (h *Handler) HandleWebSocket(c echo.Context) error {
-	userID := c.QueryParam("userID")
+    userID, ok := c.Get("user_id").(uint)
+    if !ok {
+        c.Logger().Error("Invalid user ID")
+        return echo.NewHTTPError(http.StatusInternalServerError, "Invalid user ID")
+    }
+    c.Logger().Info("User ID in HandleWebSocket:", userID)
 
-	conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
+    conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+    if err != nil {
+        return err
+    }
+    defer conn.Close()
 
-	client := &Client{
-		UserID: userID,
-		Conn:   conn,
-	}
-	clients[userID] = client
+    client := &Client{
+        UserID: userID,
+        Conn:   conn,
+    }
+    clients[userID] = client
 
-	for {
-		var message Message
-		err := conn.ReadJSON(&message)
-		if err != nil {
-			log.Println(err)
-			break
-		}
+    for {
+        var message Message
+        err := conn.ReadJSON(&message)
+        if err != nil {
+            log.Println(err)
+            break
+        }
 
-		recipientClient, ok := clients[message.RecipientID]
-		if !ok {
-			log.Printf("Recipient %s not found", message.RecipientID)
-			continue
-		}
+        recipientID, err := strconv.ParseUint(message.RecipientID, 10, 64)
+        if err != nil {
+            log.Printf("Invalid recipient ID: %v", err)
+            continue
+        }
 
-		if err := recipientClient.Conn.WriteJSON(message); err != nil {
-			log.Println(err)
-		}
-	}
+        recipientClient, ok := clients[uint(recipientID)]
+        if !ok {
+            log.Printf("Recipient %d not found", recipientID)
+            continue
+        }
 
+        if err := recipientClient.Conn.WriteJSON(message); err != nil {
+            log.Println(err)
+        }
+    }
 	delete(clients, userID)
 
 	return nil
